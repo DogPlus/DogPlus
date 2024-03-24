3e100e9d1eb998422898c63c56ec8a251c187286
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2024-03-24 18:14:52 +0100

    Add user registration and logout functionality

--- backend/django_backend/authentication/views.py
+++ backend/django_backend/authentication/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterUserAPIView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response(
+                {**serializer.data, "token": token.key},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class Logout(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        token = get_object_or_404(Token, user=request.user)
+        token.delete()
+        return Response({"detail": "Successfully logged out."}, status=status.HTTP_204_NO_CONTENT)
