7fe569edf736e643a8d267a7189eb43b3292c351
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2024-03-24 18:15:23 +0100

    feat: add CustomUserAdmin class and role field to CustomUser model

--- backend/django_backend/authentication/models.py
+++ backend/django_backend/authentication/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    USER = 1
+    SERVICE_PROVIDER = 2
+    ADMIN = 3
+    ROLE_CHOICES = (
+        (USER, 'User'),
+        (SERVICE_PROVIDER, 'Service Provider'),
+        (ADMIN, 'Admin'),
+    )
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, default=USER)
