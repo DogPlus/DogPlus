6526bb65afcb8625b0376f7c852fabeebc64547f
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2024-03-26 08:49:23 +0100

    fix: add email field to UserSerializer

--- backend/django_backend/authentication/serializers.py
+++ backend/django_backend/authentication/serializers.py
@@ -6,7 +6,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'role')
+        fields = ('id', 'username', 'email', 'password', 'role')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
