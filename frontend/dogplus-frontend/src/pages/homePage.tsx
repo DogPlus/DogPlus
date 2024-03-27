e7d30a265636b460be970afe21df120615a6a856
Brage Minge <bragmin@hotmail.com>
2024-03-27 16:29:51 +0100

    added Feed component to homepage

--- frontend/dogplus-frontend/src/pages/homePage.tsx
+++ frontend/dogplus-frontend/src/pages/homePage.tsx
@@ -1,11 +1,15 @@
 // Create a simple react page
 
 import React from 'react';
+import Feed from '../components/Feed';
 
 export const HomePage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  );
+    <div>
+
+    <Feed /> 
+
+    </div>
+
+);
 };
