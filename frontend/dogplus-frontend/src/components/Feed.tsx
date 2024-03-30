f638273c92c6e459f5921f0f8f077d9f67e9d73c
Brage Minge <bragmin@hotmail.com>
2024-03-30 17:39:05 +0100

    Added a bit of styling to the feed and posts

--- frontend/dogplus-frontend/src/components/Feed.tsx
+++ frontend/dogplus-frontend/src/components/Feed.tsx
@@ -4,6 +4,8 @@ import postsData from '../assets/fakePosts.json';
 interface Post {
   id: number;
   author: string;
+  profile_pic: string;
+  date: string;
   text: string;
   image?: string;
 }
@@ -18,13 +20,45 @@ const Feed: React.FC = () => {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Feed</h1>
+      <h1 className="text-3xl font-bold ">Dog+</h1>
       {posts.map(post => (
-        <div key={post.id}>
-          <h2>{post.author}</h2>
-          <p>{post.text}</p>
-          {post.image && <img src={post.image} alt="Post" />}
+
+    <div key={post.id} className="bg-white rounded-lg shadow-md border p-4 mt-4">
+    <div className="flex items-center mb-4">
+        <img className="w-12 h-12 rounded-full mr-3" src={post.profile_pic} alt="Profile Image" />
+        <div>
+            <h2 className="text-lg font-semibold">{post.author}</h2>
+            <p className="text-gray-500 text-sm">Published on {post.date}</p>
         </div>
+    </div>
+    <p className="text-gray-700 mb-4">
+    {post.text}
+    </p>
+    {post.image && <img src={post.image} alt="Post" />}
+
+    {/* Like and Comment Icons */}
+    <div className="pt-4 mt-4 border-t border-gray-200">
+        <div className="flex justify-between items-center">
+            <button className="flex items-center mr-4 text-gray-700 hover:text-blue-600">
+                <i className="fas fa-thumbs-up mr-1"></i>
+                <span>Like</span>
+            </button>
+            <button className="flex items-center text-gray-700 hover:text-green-600">
+              <i className="far fa-comment mr-1"></i>
+              <span>Comment</span>
+            </button>
+
+        </div>
+    </div>
+
+    <div className="flex justify-end">
+        {/* <a href="#" className="text-blue-500 font-semibold">Read More</a> */}
+    </div>
+    </div>
+
+
+
+
       ))}
     </div> 
   );
