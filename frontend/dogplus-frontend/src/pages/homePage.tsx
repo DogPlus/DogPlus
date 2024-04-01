6a9220642837d5c2b5fdaaab91661646d54d06c1
Brage Minge <bragmin@hotmail.com>
2024-04-01 21:33:28 +0200

    posts are shown in chronological order and added to feed as props

--- frontend/dogplus-frontend/src/pages/homePage.tsx
+++ frontend/dogplus-frontend/src/pages/homePage.tsx
@@ -1,15 +1,20 @@
-// Create a simple react page
-
-import React from 'react';
+import React, { useState } from 'react';
 import Feed from '../components/Feed';
+import { AddPostButton } from '../components/AddPostButton';
+import initialPosts from '../assets/fakePosts.json';
+import { Post } from '../assets/interfaces';
 
 export const HomePage = () => {
+  const [posts, setPosts] = useState<Post[]>(initialPosts);
+
+  const handleAddPost = (newPost: Post) => {
+    setPosts([newPost, ...posts]); // Add the new post to the end of the posts list
+  };
+
   return (
     <div>
-
-    <Feed /> 
-
+      <AddPostButton onAddPost={handleAddPost} />
+      <Feed posts={posts} />
     </div>
-
-);
+  );
 };
