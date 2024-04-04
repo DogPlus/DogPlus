9a72a240d7920eb406ea07290d4e3026ca1ffdb8
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2024-04-04 14:21:38 +0200

    Add corsheaders middleware to settings.py

--- backend/django_backend/django_backend/settings.py
+++ backend/django_backend/django_backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'django_backend.urls'
