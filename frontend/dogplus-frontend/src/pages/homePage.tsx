ae3482c5a3a0d397c9d1850ebb39d1035d0badd8
Brage Minge <bragmin@hotmail.com>
2024-04-09 14:30:03 +0200

    now displays posts from backend. something wrong with the url in line 17

--- frontend/dogplus-frontend/src/pages/homePage.tsx
+++ frontend/dogplus-frontend/src/pages/homePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Feed from '../components/Feed';
 import { AddPostButton } from '../components/AddPostButton';
 import initialPosts from '../assets/fakePosts.json';
@@ -10,6 +10,29 @@ export const HomePage = () => {
   const handleAddPost = (newPost: Post) => {
     setPosts([newPost, ...posts]); // Add the new post to the end of the posts list
   };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_HOST}/api/feed/create-post/`, { // jeg skjønner ikke hvorfor det er /api/feed/create-post/ her
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Token "+ localStorage.getItem("token") //må kanskje endres?
+          },
+        });
+
+        if (!response.ok) throw new Error('Failed to fetch posts')
+        const data = await response.json();
+      
+        setPosts(data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
 
   return (
     <div>
