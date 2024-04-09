350d31fa90fec956bfc60778dbb566b154a44138
Brage Minge <bragmin@hotmail.com>
2024-04-09 14:28:47 +0200

    added a view for getting all posts from a user

--- backend/django_backend/feed/views.py
+++ backend/django_backend/feed/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from .models import Post
 from .serializers import PostSerializer
+from django.contrib.auth.models import User
 
 class CreatePostAPIView(APIView):
     def get(self, request, format=None):
@@ -20,3 +21,11 @@ class CreatePostAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class AllPostsFromSpecificUserAPIView(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        user = get_object_or_404(User, pk=user_id)
+        return Post.objects.filter(author=user)
