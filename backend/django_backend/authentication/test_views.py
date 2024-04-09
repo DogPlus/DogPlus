2a91d2aec28a1e437fcc095a03d2a907757a23a4
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2024-04-09 16:34:26 +0200

    test: add user registration and login tests

--- backend/django_backend/authentication/test_views.py
+++ backend/django_backend/authentication/test_views.py
@@ -8,47 +8,46 @@ User = get_user_model()
 
 class UserViewTests(APITestCase):
     def setUp(self):
-        self.create_url = reverse('register')
+        self.register_url = reverse('register')
+        self.login_url = reverse('login')
         self.logout_url = reverse('logout')
-        # Create a test user
-        self.test_user = User.objects.create_user(username='testuser', email='test@example.com', password='testpassword123')
-        # Get or create a token for the test user
+        self.test_user = User.objects.create_user(username='testuser', email='test@example.com', password='testpassword123', is_approved=True)
         self.token, _ = Token.objects.get_or_create(user=self.test_user)
-        # Set the token in the client's headers for authenticated requests
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
-
-    def test_create_user(self):
+    def test_register_user(self):
         """
-        Ensure we can create a new user and receive a token.
+        Ensure we can register a new user.
         """
         data = {
             'username': 'newuser',
             'email': 'newuser@example.com',
             'password': 'newpassword123',
+            'registerAsServiceProvider': False,  # Register as a regular user
         }
 
-        response = self.client.post(self.create_url, data, format='json')
-        user = User.objects.latest('id')
-        token = Token.objects.get(user=user)
-
+        response = self.client.post(self.register_url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['email'], data['email'])
-        self.assertEqual(response.data['username'], data['username'])
         self.assertTrue('token' in response.data)
-        self.assertEqual(response.data['token'], token.key)
 
-    def test_logout(self):
+    def test_login_user(self):
         """
-        Ensure we can logout successfully and token is deleted.
+        Ensure we can login a user and receive a token.
         """
-        # First, authenticate with the test user's token
-        token, created = Token.objects.get_or_create(user=self.test_user)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword123',
+        }
 
-        # Attempt to logout
+        response = self.client.post(self.login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue('token' in response.data)
+
+    def test_logout_user(self):
+        """
+        Ensure we can logout a user successfully.
+        """
         response = self.client.post(self.logout_url)
-
-        # Check that the response indicates success and the token is deleted
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Token.objects.filter(user=self.test_user).exists())
+
