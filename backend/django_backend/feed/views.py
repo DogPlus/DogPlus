32deafd5f2ef126a2224573ad281bcbdd5f7788b
Vegard Sjåvik <vgsjaavik@gmail.com>
2024-04-11 13:44:16 +0200

    [DP-34] Add endpoints for liking/disliking

--- backend/django_backend/feed/views.py
+++ backend/django_backend/feed/views.py
@@ -28,3 +28,34 @@ class AllPostsFromSpecificUserAPIView(generics.ListAPIView):
     def get_queryset(self):
         user_id = self.kwargs['user_id']
         return Post.objects.filter(author=user_id).order_by('-date_posted')
+
+class LikePostAPIView(APIView):
+    def post(self, request, post_id, format=None):
+        post = get_object_or_404(Post, pk=post_id)
+        user = request.user
+
+        # Add like
+        post.liked_by.add(user)
+        post.like_count += 1
+        post.save()
+        return Response({'message': 'Like added successfully.'}, status=status.HTTP_200_OK)
+
+    def delete(self, request, post_id, format=None):
+        post = get_object_or_404(Post, pk=post_id)
+        user = request.user
+
+        # Remove like
+        if user in post.liked_by.all():
+            post.liked_by.remove(user)
+            post.like_count -= 1
+            post.save()
+            return Response({'message': 'Like removed successfully.'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'User has not liked this post.'}, status=status.HTTP_400_BAD_REQUEST)
+
+class LikedPostsByUserAPIView(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        return Post.objects.filter(liked_by=user_id).order_by('-date_posted')
