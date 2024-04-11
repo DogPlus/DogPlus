a98e2427985cce1e735eaaab900e60b928429b96
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2024-04-11 17:11:41 +0200

    feat: add ServiceCreateUpdateView and ServiceProviderDashboardView to services/views.py

--- backend/django_backend/services/views.py
+++ backend/django_backend/services/views.py
@@ -1,26 +1,48 @@
-# services/views.py
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-
 from backend.django_backend.authentication.permissions import IsServiceProvider
+from backend.django_backend.bookings.models import Booking
+from backend.django_backend.bookings.serializers import BookingSerializer
 from .models import Service
 from .serializers import ServiceSerializer
-from bookings.models import Booking
-from bookings.serializers import BookingSerializer
+from rest_framework import status
+
+class ServiceCreateUpdateView(APIView):
+    permission_classes = [IsAuthenticated, IsServiceProvider]
+
+    def post(self, request):
+        # Service creation
+        serializer = ServiceSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save(service_provider=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request):
+        # Update existing service
+        service = get_object_or_404(Service, service_provider=request.user)
+        serializer = ServiceSerializer(service, data=request.data, partial=True, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ServiceProviderDashboardView(APIView):
     permission_classes = [IsAuthenticated, IsServiceProvider]
 
     def get(self, request):
+        # Retrieve service provider's service and bookings
         user = request.user
         service = get_object_or_404(Service, service_provider=user)
         bookings = Booking.objects.filter(service_provider=user).order_by('-booking_date')
-        
+
         service_data = ServiceSerializer(service).data
         bookings_data = BookingSerializer(bookings, many=True).data
-        
+
         return Response({
             'service': service_data,
             'bookings': bookings_data
