e0576bddc024c95562f2f1aedd32a240f04f1d58
Vegard Sjåvik <vgsjaavik@gmail.com>
2024-04-11 14:09:35 +0200

    [DP-34] Frontend for liking posts

--- frontend/dogplus-frontend/src/components/Feed.tsx
+++ frontend/dogplus-frontend/src/components/Feed.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Post } from '../types/post'
 import { PostCard } from './Post';
@@ -8,12 +8,44 @@ interface FeedProps {
 }
 
 const Feed: React.FC<FeedProps> = ({ posts }) => {
+  const [likedPosts, setLikedPosts] = React.useState<Post[]>([]);
+
+  const user_id = 1
+
+  useEffect(() => {
+    const fetchLikedPosts = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_HOST}/api/feed/posts/liked_by/${user_id}`, {
+          method: "GET",
+          headers: {
+            "Authorization": `Token ${localStorage.getItem("token")}`
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        setLikedPosts(data);
+      } catch (error) {
+        console.error("Error fetching liked posts:", error);
+      }
+    };
+
+    fetchLikedPosts(); // Call the async function immediately
+
+  }, [user_id]);
+
+  const isLiked = (post: Post) => {
+    return likedPosts.some(likedPost => likedPost.id === post.id);
+  }
 
   return (
     <div className="pb-20">
       <h1 className="text-3xl font-bold ">Dog+</h1>
       {posts.map(post => (
-        <PostCard key={post.id} post={post} />
+        <PostCard key={post.id} post={post} isLiked={isLiked(post)}/>
       ))
       }
     </div> 
