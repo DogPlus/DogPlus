9f88f9eca0d91d65b7bf27dae00014ef4083442b
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2024-04-11 17:11:14 +0200

    feat: update ServiceSerializer fields and add validation

--- backend/django_backend/services/serializers.py
+++ backend/django_backend/services/serializers.py
@@ -1,8 +1,17 @@
-# services/serializers.py
 from rest_framework import serializers
 from .models import Service
 
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'location', 'service_provider', 'fixed_price', 'price_per_session', 'session_time']
+        read_only_fields = ['service_provider', 'price_type'] 
+
+    def validate(self, data):
+        if data['name'] in [Service.DOG_TRAINING, Service.DOG_DAYCARE]:
+            if not data.get('price_per_session') or not data.get('session_time'):
+                raise serializers.ValidationError("Price per session and session time must be provided for training and daycare services.")
+        else:
+            if not data.get('fixed_price'):
+                raise serializers.ValidationError("A fixed price must be provided for insurance and veterinary services.")
+        return data
