eef15bf7d0f3830a90d116bb047d1631b485e465
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2024-04-15 12:54:51 +0200

    migrate: refactor authentication permissions and add initial migrations for bookings and services

--- backend/django_backend/authentication/permissions.py
+++ backend/django_backend/authentication/permissions.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions
-from .models import CustomUser
+
+
 
 class IsServiceProvider(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == CustomUser.SERVICE_PROVIDER
+        return request.user.is_authenticated and request.user.role == "SERVICE_PROVIDER"
