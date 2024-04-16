1494d378f51e4b8ab2154ca7d02eb12f4d21b614
Vegard Sjåvik <vgsjaavik@gmail.com>
2024-04-16 11:43:28 +0200

    Get available timeslots skeleton

--- frontend/dogplus-frontend/src/components/common/timeselector.tsx
+++ frontend/dogplus-frontend/src/components/common/timeselector.tsx
@@ -10,7 +10,7 @@ const TimeSelector: React.FC<TimeSelectorProps> = ({ selectedTime, onTimeChange}
   const generateTimeOptions = (): string[] => {
     const options: string[] = [];
     for (let hour = 7; hour < 24; hour++) {
-      for (let minute = 0; minute < 60; minute += 15) {
+      for (let minute = 0; minute < 60; minute += 30) {
         const formattedHour = hour.toString().padStart(2, '0');
         const formattedMinute = minute.toString().padStart(2, '0');
         options.push(`${formattedHour}:${formattedMinute}`);
--- frontend/dogplus-frontend/src/pages/serviceProviderBookingPage.tsx
+++ frontend/dogplus-frontend/src/pages/serviceProviderBookingPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Datepicker } from "flowbite-react";
 import TimePicker from '../components/common/timepicker';
 import TimeSelector from '../components/common/timeselector';
@@ -6,39 +6,26 @@ import TimeSelector from '../components/common/timeselector';
 export const ServiceProviderBookingPage = () => {
   const [selectedStartTime, setSelectedStartTime]= React.useState<string>("12:00");
   const [selectedTime, setSelectedTime]= React.useState<string | null>(null);
+  const [availableTimeslots, setAvailableTimeslots]= React.useState<string[]>([]);
 
   const onTimeChange = (time: string): any => {
     setSelectedTime(time);
   };
 
-  const availableTimes = [
-    "08:00",
-    "08:30",
-    "09:00",
-    "09:30",
-    "11:00",
-    "13:30",
-    "14:00",
-    "16:30",
-    "17:00",
-    "18:00",
-    "18:30",
-    "19:30",
-    "20:00",
-  ];
+  // TODO: Fetch avaialable times for the selected date
+  useEffect(() => {
+    // timeslots = fetchAvailableTimes();
+    setAvailableTimeslots([]);
+  }, [selectedTime]);
 
   return (
     <div className="m-3">
-      <h1 className="text-3xl font-bold underline">
-        Service Providers Booking 
-      </h1>
-
       <div className="grid grid-cols-2 gap-4 mb-3">
         <Datepicker />
         <TimeSelector selectedTime={selectedStartTime} onTimeChange={setSelectedStartTime} />
       </div>
       <div className="mb-3">
-        <TimePicker selectedTime={selectedTime} startTime={selectedStartTime} availableTimes={availableTimes} onTimeChange={onTimeChange} />
+        <TimePicker selectedTime={selectedTime} startTime={selectedStartTime} availableTimes={availableTimeslots} onTimeChange={onTimeChange} />
       </div>
       <button
         className="w-full px-6 py-5 mb-5 text-sm font-bold leading-none text-white transition duration-300 md:w-96 rounded-2xl hover:bg-purple-blue-600 focus:ring-4 focus:ring-purple-blue-100 bg-purple-blue-500"
