e5b6af9a243422d4532e80d1356325754fa0adfa
Brage Minge <bragmin@hotmail.com>
2024-04-20 11:48:14 +0200

    created a (service)provider type

---
+++ frontend/dogplus-frontend/src/types/provider.ts
@@ -0,0 +1,8 @@
+export interface Provider {
+    id: string;
+    username: string;
+    email: string;
+    isApproved?: boolean; //this value doesnt affect the user, only admin/superuser, could be excluded
+    serviceProviderKey?: string; //this value doesnt affect the user, only admin/superuser, could be excluded
+    displayName?: string;
+}
