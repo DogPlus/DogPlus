5e20a3d9624e4fdbb2b3bf3745f8f4e5f99b6a95
Brage Minge <bragmin@hotmail.com>
2024-04-20 12:11:35 +0200

    renamed to serviceprovider

--- frontend/dogplus-frontend/src/types/serivceProvider.ts
+++
@@ -1,8 +0,0 @@
-export interface Provider {
-    id: string;
-    username: string;
-    email: string;
-    isApproved?: boolean; //this value doesnt affect the user, only admin/superuser, could be excluded
-    serviceProviderKey?: string; //this value doesnt affect the user, only admin/superuser, could be excluded
-    displayName?: string;
-}
