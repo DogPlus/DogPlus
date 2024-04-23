b409de754a822104ed367c17011d7788af011c61
VegSja <vgsjaavik@gmail.com>
2024-04-23 20:54:22 +0200

    [DP-47] Some styling

--- frontend/dogplus-frontend/src/InstallPwa.tsx
+++ frontend/dogplus-frontend/src/InstallPwa.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 const InstallPWA: React.FC = () => {
+  const [showModal, setShowModal] = useState(true);
   const [deferredPrompt, setDeferredPrompt] = useState<Event | null>(null);
 
   useEffect(() => {
@@ -16,6 +17,10 @@ const InstallPWA: React.FC = () => {
     };
   }, []);
 
+  useEffect(() => {
+    setShowModal(deferredPrompt !== null);
+  }, [deferredPrompt]);
+
   const handleInstallClick = () => {
     if (deferredPrompt) {
       (deferredPrompt as any).prompt();
@@ -30,10 +35,35 @@ const InstallPWA: React.FC = () => {
     }
   };
 
+  const handleCloseModal = (e: React.MouseEvent) => {
+      if (e.target === e.currentTarget) {
+          setShowModal(false);
+      }
+  };
+
   return (
-    <button onClick={handleInstallClick} style={{ display: deferredPrompt ? 'block' : 'none' }}>
-      Install App
-    </button>
+    <>
+      {showModal && (
+        <div onClick={handleCloseModal} className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
+          <div className="relative top-20 mx-auto mt-20 p-5 border w-96 shadow-lg rounded-md bg-white">
+
+            <button 
+              onClick={() => setShowModal(false)} 
+              className="absolute top-0 right-0 mt-2 mr-2 text-gray-700 hover:text-gray-900">
+              <i className="fas fa-times"></i> 
+            </button>
+            
+            <p>Dog+ can be installed app to get the best possible experience!</p>
+
+            <div>
+              <button onClick={handleInstallClick} className="mt-4 bg-blue-500 text-white p-2 rounded">
+                Install App
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
