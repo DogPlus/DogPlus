db578f931556686a6249d132bad27f10e87954c7
VegSja <vgsjaavik@gmail.com>
2024-04-23 19:19:19 +0200

    [DP-37] Fix issue where navbar hides buttons

--- frontend/dogplus-frontend/src/components/common/navbar.tsx
+++ frontend/dogplus-frontend/src/components/common/navbar.tsx
@@ -14,10 +14,9 @@ const navlink_active =
 
 export const Navbar = () => {
   const { user } = useUser();
-  console.log(user);
   return (
-    <div className="fixed bottom-0 left-0 py-1 px-1 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
-      <nav className="grid h-full max-w-lg grid-cols-3 mx-auto font-medium gap-4">
+    <div className="py-1 px-1 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+        <nav className="grid h-full max-w-lg grid-cols-3 mx-auto font-medium gap-4">
         <NavLink
           to="/home"
           className={({ isActive }) =>
