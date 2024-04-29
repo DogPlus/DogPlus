d3a7745f4ca34064755d4a36f13e6b86fbf41645
Brage Minge <bragmin@hotmail.com>
2024-04-29 20:08:12 +0200

    refactored so that the list now takes the serviceproviders as props instead of fetching inside the list component

--- frontend/dogplus-frontend/src/components/ServiceProviderList.tsx
+++ frontend/dogplus-frontend/src/components/ServiceProviderList.tsx
@@ -5,32 +5,12 @@ import ProviderCard from './ServiceProviderCard';  // Make sure to import the Pr
 import { ServiceProvider } from '../types/serviceProvider';
 import { useNavigate } from 'react-router-dom';
 
-const ServiceProviderList: React.FC = () => {
-    const [serviceProviders, setServiceProviders] = useState<ServiceProvider[]>([]);
+interface ServiceProviderListProps {
+    serviceProviders: ServiceProvider[];  // Define the type for the incoming prop
+}
+
+const ServiceProviderList: React.FC<ServiceProviderListProps> = ({serviceProviders}) => {
     const navigate = useNavigate();
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`${process.env.REACT_APP_BACKEND_HOST}/api/providers/service-providers/list/`,{ //TODO change to an endpoint using a user specific algorithm
-                    method: "GET",
-                    headers: {
-                      "Authorization": "Token "+ localStorage.getItem("token")
-                    },
-                  });
-
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data: ServiceProvider[] = await response.json();
-                setServiceProviders(data);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-
-        fetchData();
-    }, []);
 
     return (
         <div>
