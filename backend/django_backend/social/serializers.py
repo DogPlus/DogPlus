1b7f5632f64cbc5c0536f6c5ebe79a7dd90b567e
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2024-04-29 17:14:04 +0200

    Refactor FollowSerializer in social app to include follower and followed details

--- backend/django_backend/social/serializers.py
+++ backend/django_backend/social/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
-from .models import Follow, Message
+from .models import Follow
+from authentication.serializers import UserDetailSerializer 
 
 class FollowSerializer(serializers.ModelSerializer):
+    follower_detail = UserDetailSerializer(source='follower', read_only=True)
+    followed_detail = UserDetailSerializer(source='followed', read_only=True)
+
     class Meta:
         model = Follow
-        fields = '__all__'
+        fields = ['id', 'follower', 'followed', 'is_accepted', 'follower_detail', 'followed_detail']
 
-class MessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Message
-        fields = '__all__'
