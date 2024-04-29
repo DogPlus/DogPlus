9e675a7b6ecf64ef444dbe39f22d02771dd99b0c
Brage Minge <bragmin@hotmail.com>
2024-04-29 20:08:56 +0200

    now fetches the serviceproviders here, and added the search component

--- frontend/dogplus-frontend/src/pages/serviceProviderPage.tsx
+++ frontend/dogplus-frontend/src/pages/serviceProviderPage.tsx
@@ -1,11 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import ServiceProviderList from "../components/ServiceProviderList";
+import ServiceProviderSearch from "../components/ServiceProviderSearch";
+import { ServiceProvider } from '../types/serviceProvider';
+
 
 export const ServiceProviderPage = () => {
+  const [serviceProviders, setServiceProviders] = useState<ServiceProvider[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_BACKEND_HOST}/api/providers/service-providers/list/`,{ //TODO change to an endpoint using a user specific algorithm
+                method: "GET",
+                headers: {
+                  "Authorization": "Token "+ localStorage.getItem("token")
+                },
+              });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data: ServiceProvider[] = await response.json();
+            setServiceProviders(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    fetchData();
+}, []);
+
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Service Providers Page</h1>
-      <ServiceProviderList />
+      <h1 className="text-3xl font-bold underline">Service Providers</h1>
+      <ServiceProviderSearch onSearchResults={setServiceProviders} />
+      <ServiceProviderList serviceProviders={serviceProviders} />
     </div>
-
   );
 };
