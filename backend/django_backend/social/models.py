d2bcacd03765e6ace04dcf4b5b94207a5c6dc34e
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2024-04-29 17:13:58 +0200

    Add is_accepted field to Follow model in social app

--- backend/django_backend/social/models.py
+++ backend/django_backend/social/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 class Follow(models.Model):
     follower = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="following", on_delete=models.CASCADE)
     followed = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="followers", on_delete=models.CASCADE)
+    is_accepted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
