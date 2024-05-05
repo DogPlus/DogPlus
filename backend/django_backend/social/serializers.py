20cdce28c4ab2eb8b01b7707caeadca7b94ba9e0
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2024-05-05 13:08:24 +0200

    chore: Refactor FollowSerializer in social/serializers.py

--- backend/django_backend/social/serializers.py
+++ backend/django_backend/social/serializers.py
@@ -3,10 +3,10 @@ from .models import Follow
 from authentication.serializers import UserDetailSerializer 
 
 class FollowSerializer(serializers.ModelSerializer):
-    follower_detail = UserDetailSerializer(source='follower', read_only=True)
-    followed_detail = UserDetailSerializer(source='followed', read_only=True)
+    follower = UserDetailSerializer(read_only=True)
+    followed = UserDetailSerializer(read_only=True)
 
     class Meta:
         model = Follow
-        fields = ['id', 'follower', 'followed', 'is_accepted', 'follower_detail', 'followed_detail']
+        fields = ['id', 'follower', 'followed', 'is_accepted']
 
