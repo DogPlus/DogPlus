6bfc743403051696cbae790bbdfc28cbdd5dc9a3
Brage Minge <bragmin@hotmail.com>
2024-05-07 18:45:30 +0200

    added the new list endpoint

--- backend/django_backend/services/urls.py
+++ backend/django_backend/services/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ServiceCreateUpdateView, ServiceDetail, ServiceProviderDashboardView, ServiceProviderServiceListView
+from .views import ServiceCreateUpdateView, ServiceDetail, ServiceProviderDashboardView, ServiceProviderServiceListView, ServiceListView
 urlpatterns = [
     path('<uuid:service_provider_id>/service/', ServiceCreateUpdateView.as_view(), name='service_create'),
     path('<uuid:service_provider_id>/service/<int:pk>/', ServiceCreateUpdateView.as_view(), name='service_update'),
     path('service/service_provider/<uuid:service_provider_id>/', ServiceProviderServiceListView.as_view(), name='service_provider_service_list'),  # Used for listing services
     path('<int:pk>/', ServiceDetail.as_view(), name='service_detail'),  # Used for updating services
     path('dashboard/', ServiceProviderDashboardView.as_view(), name='service_provider_dashboard'),
+    path('list/', ServiceListView.as_view(), name='service_list')
 ]
