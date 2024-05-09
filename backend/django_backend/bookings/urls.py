eab8bf4a6e4bd5a17e39aa427dce13b5dbb17d78
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2024-05-09 20:40:48 +0200

    chore: Add email notification for booking cancellation

--- backend/django_backend/bookings/urls.py
+++ backend/django_backend/bookings/urls.py
@@ -3,6 +3,6 @@ from .views import AvailableBookingsView, BookingView
 
 urlpatterns = [
     path('', BookingView.as_view(), name='booking'),  # Handles posting a new booking
-    path('<int:booking_id>/', BookingView.as_view(), name='booking-detail'),  # Handles delete for a specific booking
+    path('<int:booking_id>/', BookingView.as_view(), name='booking-delete'),  # Handles delete for a specific booking
     path('available/<int:service_id>', AvailableBookingsView.as_view(), name='available-timeslots'),
 ]
