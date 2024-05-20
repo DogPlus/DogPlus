be350f23e29e7ac892490da4fcc7d41132524fac
VegSja <vgsjaavik@gmail.com>
2024-05-20 09:25:09 +0200

    [DP-51] Add validators for video field

--- backend/django_backend/feed/models.py
+++ backend/django_backend/feed/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
+
+def validate_file_size(file):
+    max_size_mb = 50
+    if file.size > max_size_mb * 1024 * 1024:
+        raise ValidationError(f"Max file size is {max_size_mb}MB")
 
 
-# Create your models here.
+def validate_video_file_type(file):
+    valid_mime_types = ['video/mp4', 'video/mpeg']
+    file_mime_type = file.file.content_type
+    if file_mime_type not in valid_mime_types:
+        raise ValidationError('Unsupported file type. Only MP4 and MPEG are allowed.')
 
 # need to workout a profile_pic solution
 class Post(models.Model):
@@ -12,7 +22,7 @@ class Post(models.Model):
     )
     text = models.TextField()
     image = models.ImageField(max_length=200, upload_to='post_images/', blank=True, null=True) 
-    video = models.FileField(max_length=400,  upload_to='post_videos/', blank=True, null=True) 
+    video = models.FileField(max_length=400,  upload_to='post_videos/', blank=True, null=True, validators=[validate_file_size, validate_video_file_type]) 
     date_posted = models.DateTimeField(auto_now_add=True)
     like_count = models.IntegerField(default=0)
     liked_by = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_posts', blank=True)
